cbc443156834373183fd14f3e9387cc8e698a77b
A.Barkhanskii <123398574+Barkhanskii-A@users.noreply.github.com>
2023-03-06 09:05:24 +0300

    768px
    адаптив под 768px

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -1,12 +1,32 @@
 const html = document.documentElement;
-
-// FLS (Full Logging System) =================================================================================================================
+// logger (Full Logging System) =================================================================================================================
 function FLS(message) {
-	setTimeout(() => {
-		if (window.FLS) {
-			console.log(message);
-		}
-	}, 0);
+	// @ts-ignore
+	setTimeout(() => (window.FLS ? console.log(message) : null), 0);
+}
+/* Проверка мобильного браузера */
+const isMobile = {
+	Android: () => navigator.userAgent.match(/Android/i),
+	BlackBerry: () => navigator.userAgent.match(/BlackBerry/i),
+	iOS: () => navigator.userAgent.match(/iPhone|iPad|iPod/i),
+	Opera: () => navigator.userAgent.match(/Opera Mini/i),
+	Windows: () => navigator.userAgent.match(/IEMobile/i),
+	any: () => isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows(),
+};
+/* Добавление класса touch для HTML если браузер мобильный */
+function addTouchClass() {
+	// Добавление класса _touch для HTML если браузер мобильный
+	if (isMobile.any()) {
+		html.classList.add('touch');
+	}
+}
+// Добавление loaded для HTML после полной загрузки страницы
+function addLoadedClass() {
+	window.addEventListener('load', () => {
+		setTimeout(() => {
+			html.classList.add('loaded');
+		}, 0);
+	});
 }
 
 // Модуль работы со слайдером =====================================================================================================================
@@ -16,3 +36,5 @@ const swiper = new Swiper('.swiper', {
 		prevEl: '.swiper-button-disabled',
 	},
 });
+
+export { FLS, isMobile, addTouchClass, addLoadedClass };
